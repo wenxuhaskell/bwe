36c3dc9d68195a7d068cf48671bc591c6ee42f5f
wen <wenxukth@gmail.com>
2024-01-05 14:39:19 +0100

    Add dimensionality reduction using PCA.

---
+++ createSmallDataSet.py
@@ -0,0 +1,137 @@
+import os
+import time
+import argparse
+import json
+from typing import Dict, Optional, Tuple
+import numpy as np
+from tqdm import tqdm
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from BweReward import RewardFunction
+
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
+def load_data(datafile: os.PathLike | str) -> Optional[Dict]:
+    try:
+        with open(datafile, 'r') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        print(f"File not found: {datafile}")
+        return None
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON in file {datafile}: {e}")
+        return None
+    return data
+
+def load_train_data(
+    datafile: os.PathLike | str,
+) -> Optional[Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]]:
+    data = load_data(datafile)
+    if data is None:
+        return None
+
+    bandwidth_predictions = np.array(data['bandwidth_predictions'])
+    observations = np.array(data['observations'])
+    video_quality = np.array(data['video_quality'])
+    audio_quality = np.array(data['audio_quality'])
+    return observations, bandwidth_predictions, video_quality, audio_quality
+
+
+def process_file(filename: str) -> (np.ndarray, np.ndarray, np.ndarray, np.ndarray):
+    # load the log file and prepare the dataset
+    observations_file, actions_file, _, _ = load_train_data(filename)
+    assert len(observations_file) > 0, f"File {filename} is empty"
+    # terminals are not used so they should be non 1.0
+    terminals_file = np.zeros(len(observations_file))
+    terminals_file[-1] = 1
+#    terminals_file = np.random.randint(2, size=len(observations_file))
+    return observations_file, actions_file, terminals_file
+
+def process_feature(feature: np.ndarray) -> np.ndarray:
+    # scale the feature dataset
+    scaling = StandardScaler()
+    scaling.fit(feature)
+    scaled_data = scaling.transform(feature)
+    # dimensionality reduction using PCA
+    principal = PCA(n_components=8)
+    principal.fit(scaled_data)
+    x = principal.transform(scaled_data)
+
+    return x
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--idir", type=str, default="data")
+    parser.add_argument("-m", "--maxfiles", type=int, default=20)
+    parser.add_argument("-o", "--odir", type=str, default="../data_np_small")
+    parser.add_argument("-r", "--rewardfunc", type=str, default="QOE_V1")
+
+    args = parser.parse_args()
+
+    # load the list of log files under the given directory
+    # iterate over files in that directory
+    files = sorted(os.listdir(args.idir))
+    all_files = []
+    train_data_files = []
+    for name in files:
+        f = os.path.join(args.idir, name)
+        # checking if it is a file
+        if os.path.isfile(f):
+            train_data_files.append(f)
+            if args.maxfiles > 0 and len(train_data_files) == args.maxfiles:
+                all_files.append(train_data_files.copy())
+                # reset the list of train data files
+                train_data_files = []
+
+    # add the last files
+    if len(train_data_files) > 0:
+        all_files.append(train_data_files)
+
+    print(f"Files to load: {len(files)}")
+    print(f"Divided into {len(all_files)} batches.")
+
+    counter = 0
+    time_used = 0
+    reward_func = RewardFunction(args.rewardfunc)
+    for train_data_files in all_files:
+        t_start = time.process_time()
+        observations = []
+        actions = []
+        terminals = []
+        rewards = []
+        # load data log and save it into .npz file
+        with ProcessPoolExecutor() as executor:
+            futures = [executor.submit(process_file, filename) for filename in train_data_files]
+            for future in tqdm(as_completed(futures), desc=f'Batch {counter+1} - Loading MDP', unit="file"):
+                result = future.result()
+                observations_file, actions_file, terminals_file = result
+                # calculate rewards
+                rewards_file = np.array([reward_func(o) for o in observations_file])
+                # PCA dimensionality reduction of the feature
+                observations_file = process_feature(observations_file)
+                # save all data from the single data log file
+                observations.append(observations_file)
+                actions.append(actions_file)
+                terminals.append(terminals_file)
+                rewards.append(rewards_file)
+
+        observations = np.concatenate(observations)
+        actions = np.concatenate(actions)
+        terminals = np.concatenate(terminals)
+        rewards = np.concatenate(rewards)
+        # create the file
+        f_name = '{:04d}'.format(counter)
+        f_o = open(f"{args.odir}/{f_name}.npz", 'wb')
+        np.savez_compressed(f_o, obs=observations, acts=actions, terms=terminals, rws=rewards)
+        f_o.close()
+        # increase the counter
+        counter = counter + 1
+        # elapsed time
+        t_end = time.process_time()
+        print(f"Time (s) for converting data log file {f_name}.npz: {t_end - t_start}")
+        time_used = time_used + (t_end-t_start)
+
+    print(f'Total time (s) used: {time_used}')
+
+if __name__ == "__main__":
+    main()
