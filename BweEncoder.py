d35ec0bf657cd81cf742a195a50d24a0c32885d6
wen <wenxukth@gmail.com>
2024-01-09 20:29:13 +0100

    Remove hidden cells fro lstm.

--- BweEncoder.py
+++ BweEncoder.py
@@ -8,37 +8,37 @@ class LSTMEncoder(nn.Module):
     def __init__(self, observation_shape, feature_size):
         super().__init__()
         self.feature_size = feature_size
-        self.fc = nn.Linear(observation_shape[0], 128).cuda()
-        self.lstm = nn.LSTM(128, feature_size, num_layers=1).cuda()
-        self.rnn_state = (torch.zeros(1, 1, 1).cuda(), torch.zeros(1, 1, 1).cuda())
+        self.fc = nn.Linear(observation_shape[0], 128)
+        self.lstm = nn.LSTM(128, feature_size, num_layers=1)
+        self.rnn_state = (torch.zeros(1, 1, 1).requires_grad_(), torch.zeros(1, 1, 1).requires_grad_())
 
-    def forward(self, inp):
+    def forward(self, inp, h=None, c=None):
         # Initialize hidden state with zeros
         # batch_size, sequence length, input dimension
         inp = torch.relu(self.fc(inp))
-        hidden_output, rnn_state_undetached = self.lstm(inp.unsqueeze(dim=0), self.rnn_state)
-        # TODO: detach?
+        hidden_output, rnn_state_undetached = self.lstm(inp.unsqueeze(dim=0))
+        # detach?
         self.rnn_state = (rnn_state_undetached[0].detach(), rnn_state_undetached[1].detach())
-        return torch.relu(hidden_output[:, -1, :]).cuda().detach()
+        return torch.relu(hidden_output[:, -1, :])
 
 
 class LSTMEncoderWithAction(nn.Module):
     def __init__(self, observation_shape, action_size, feature_size):
         super().__init__()
         self.feature_size = feature_size
-        self.fc = nn.Linear(observation_shape[0] + action_size, 128).cuda()
-        self.lstm = nn.LSTM(128, feature_size, num_layers=1).cuda()
-        self.rnn_state = (torch.zeros(1, 1, 1).cuda(), torch.zeros(1, 1, 1).cuda())
+        self.fc = nn.Linear(observation_shape[0] + action_size, 128)
+        self.lstm = nn.LSTM(128, feature_size, num_layers=1)
+        self.rnn_state = (torch.zeros(1, 1, 1).requires_grad_(), torch.zeros(1, 1, 1).requires_grad_())
 
-    def forward(self, inp, action):
+    def forward(self, inp, action, h=None, c=None):
         # Initialize hidden state with zeros
         # batch_size, sequence length, input dimension
         inp = torch.cat([inp, action], dim=1)
         inp = torch.relu(self.fc(inp))
-        hidden_output, rnn_state_undetached = self.lstm(inp.unsqueeze(dim=0), self.rnn_state)
-        # TODO: detach?
+        hidden_output, rnn_state_undetached = self.lstm(inp.unsqueeze(dim=0))
+        # detach?
         self.rnn_state = (rnn_state_undetached[0].detach(), rnn_state_undetached[1].detach())
-        return torch.relu(hidden_output[:, -1, :]).cuda().detach()
+        return torch.relu(hidden_output[:, -1, :])
 
 
 @dataclasses.dataclass()
