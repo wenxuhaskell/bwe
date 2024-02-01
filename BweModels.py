33945dd83a68c3d23e07d52d70fe7f065182b0b8
wenxuhaskell <wenxukth@gmail.com>
2024-02-01 16:45:58 +0000

    findMeanStd.py - calculate mean and std-variances of the dataset.

--- BweModels.py
+++ BweModels.py
@@ -15,6 +15,91 @@ from BweUtils import load_train_data, load_multiple_files, load_train_data_from_
 from BweLogger import BweAdapterFactory
 from BweEncoder import LSTMEncoderFactory, ACEncoderFactory
 
+OBSERVATION_MAX = []
+OBSERVATION_MIN = []
+
+OBSERVATION_MEAN = [1.93663500e+06, 1.77384042e+06,  1.78181603e+06,  1.74979287e+06,
+    1.72435037e+06,  1.76951604e+06,  1.73935227e+06,  1.73589912e+06,
+    1.72929810e+06,  1.72387474e+06,  1.61544534e+01,  1.47095189e+01,
+    1.47754257e+01,  1.45324404e+01,  1.43607080e+01,  1.47059433e+02,
+    1.44549616e+02,  1.44244884e+02,  1.43707974e+02,  1.43270752e+02,
+    1.45247625e+04,  1.33038031e+04,  1.33636202e+04,  1.31234465e+04,
+    1.29326278e+04,  1.32713703e+05,  1.30451420e+05,  1.30192434e+05,
+    1.29697357e+05,  1.29290606e+05,  2.04223799e+01,  1.76300161e+01,
+    1.84191965e+01,  1.78941613e+01,  1.81959718e+01,  2.14826214e+01,
+    2.07620545e+01,  2.08947474e+01,  2.07176323e+01,  2.06223327e+01,
+    -1.92081006e+01, -1.99729157e+01, -1.97849512e+01, -1.99318159e+01,
+    -1.98816837e+01, -1.81478591e+01, -1.87686302e+01, -1.85616910e+01,
+    -1.86214218e+01, -1.86028651e+01,  1.60369519e+02,  1.60303553e+02,
+    1.60242532e+02,  1.60187716e+02,  1.60133362e+02,  1.60369519e+02,
+    1.59775608e+02,  1.59233998e+02,  1.58701258e+02,  1.58174837e+02,
+    1.02760114e+00,  1.00521993e+00,  1.00741903e+00,  1.00472545e+00,
+    1.00590493e+00,  1.07405129e+00,  1.06662523e+00,  1.06350105e+00,
+    1.05988562e+00,  1.05613214e+00,  6.34008768e+00,  5.30865053e+00,
+    5.48488267e+00,  5.32288418e+00,  5.41449406e+00,  1.25301092e+01,
+    1.19660472e+01,  1.19457845e+01,  1.18883736e+01,  1.18035440e+01,
+    9.84065108e+00,  9.06462410e+00,  9.02335626e+00,  9.20722435e+00,
+    9.10437985e+00,  8.70757505e+00,  8.61945855e+00,  8.63192269e+00,
+    8.57910299e+00,  8.56538572e+00,  6.85616612e+00,  6.00467481e+00,
+    6.18285085e+00,  6.02991850e+00,  6.16190274e+00,  8.53323748e+00,
+    8.30901053e+00,  8.35234686e+00,  8.28807403e+00,  8.27705709e+00,
+    8.67008032e-03,  7.56531210e-03,  7.67195618e-03,  7.63833693e-03,
+    7.61576744e-03,  1.24066191e-02,  1.16975500e-02,  1.20921852e-02,
+    1.18567405e-02,  1.21014736e-02,  9.22884499e-02,  8.01101027e-02,
+    8.05330491e-02,  7.95441613e-02,  7.88345817e-02,  2.94028752e-01,
+    2.74226241e-01,  2.82895939e-01,  2.75885749e-01,  2.83594372e-01,
+    6.86789290e-01,  6.73967402e-01,  6.76804701e-01,  6.72995679e-01,
+    6.68933251e-01,  7.08784204e-01,  7.05396258e-01,  7.03355478e-01,
+    7.00646610e-01,  6.98403179e-01,  3.10089820e-01,  3.06521445e-01,
+    3.04754509e-01,  3.07098354e-01,  3.11525934e-01,  2.87361389e-01,
+    2.86836832e-01,  2.85849389e-01,  2.85247112e-01,  2.84306336e-01,
+    3.11798045e-03,  2.83029366e-03,  2.80833850e-03,  2.83100888e-03,
+    2.83922270e-03,  3.85091983e-03,  3.83702111e-03,  3.83882257e-03,
+    3.83242903e-03,  3.80277944e-03 ]
+
+OBSERVATION_STD = [ 1.46588941e+06, 1.35396780e+06, 1.36271677e+06, 1.35079649e+06,
+    1.34028090e+06, 1.00956779e+06, 1.00169491e+06, 1.00680559e+06,
+    1.00973528e+06, 1.01270744e+06, 1.04547330e+01, 9.59928206e+00,
+    9.65098537e+00, 9.56300738e+00, 9.49768058e+00, 7.24484542e+01,
+    7.21116201e+01, 7.25820201e+01, 7.29186173e+01, 7.32280977e+01,
+    1.09941706e+04, 1.01547585e+04, 1.02203757e+04, 1.01309737e+04,
+    1.00521067e+04, 7.57175840e+04, 7.51271179e+04, 7.55104195e+04,
+    7.57301457e+04, 7.59530582e+04, 7.82575160e+01, 6.14665483e+01,
+    6.23948153e+01, 6.15084154e+01, 6.21141153e+01, 8.11269903e+01,
+    7.00810514e+01, 7.44516413e+01, 7.09572642e+01, 7.07288226e+01,
+    8.91738606e+01, 7.41274444e+01, 7.47427291e+01, 7.41388138e+01,
+    7.46566234e+01, 9.15601281e+01, 8.18743065e+01, 8.54048069e+01,
+    8.22671143e+01, 8.19158124e+01, 6.26291495e+01, 6.26925612e+01,
+    6.27555500e+01, 6.28128795e+01, 6.28702507e+01, 6.26291495e+01,
+    6.32503689e+01, 6.38247351e+01, 6.43993928e+01, 6.49557211e+01,
+    2.81957857e-01, 2.39369862e-01, 2.50549727e-01, 2.27919139e-01,
+    2.55014288e-01, 7.29618478e-01, 6.98852353e-01, 7.05272289e-01,
+    6.98834832e-01, 6.91473274e-01, 1.50253971e+01, 9.65744435e+00,
+    9.85108575e+00, 9.36942235e+00, 9.35587219e+00, 3.47879642e+01,
+    2.64465764e+01, 2.70419853e+01, 2.67716184e+01, 2.61368097e+01,
+    1.77620336e+01, 1.43389516e+01, 1.39113219e+01, 1.46146362e+01,
+    1.40062774e+01, 1.49854046e+01, 1.32333677e+01, 1.41129752e+01,
+    1.35412775e+01, 1.36986533e+01, 8.61131852e+00, 5.49953909e+00,
+    5.56964351e+00, 5.39956239e+00, 5.54583411e+00, 1.13871125e+01,
+    8.73570797e+00, 9.80175187e+00, 9.11537323e+00, 8.88401182e+00,
+    6.21616870e-02, 5.88193642e-02, 5.92090075e-02, 5.92268596e-02,
+    5.91562715e-02, 5.64244438e-02, 5.49164749e-02, 5.60001514e-02,
+    5.55122214e-02, 5.59186441e-02, 1.42131108e+00, 1.28729170e+00,
+    1.30655077e+00, 1.27927366e+00, 1.29794311e+00, 2.62500676e+00,
+    2.56219221e+00, 2.57543585e+00, 2.55304079e+00, 2.55515906e+00,
+    2.88065833e-01, 3.08445793e-01, 3.03559454e-01, 3.06735707e-01,
+    3.05860025e-01, 2.55874175e-01, 2.59746379e-01, 2.62365488e-01,
+    2.65487511e-01, 2.68065214e-01, 2.84847474e-01, 2.95694714e-01,
+    2.91137813e-01, 2.93711906e-01, 2.93304053e-01, 2.50782002e-01,
+    2.51515555e-01, 2.51734091e-01, 2.52313232e-01, 2.52483261e-01,
+    3.94003935e-02, 3.90592867e-02, 3.87900218e-02, 3.90514751e-02,
+    3.90804436e-02, 2.14171127e-02, 2.16734777e-02, 2.18111491e-02,
+    2.17950330e-02, 2.17156514e-02 ]
+
+ACTION_MAX = 62000000.0
+ACTION_MIN = 0.0
+REWARD_MAX = 6.0
+REWARD_MIN = 1.0
 class BweDrl:
     def __init__(self, params):
         self._algo = None
@@ -381,9 +466,9 @@ def createCQL(params):
         n_action_samples=_n_action_samples,
         actor_encoder_factory=ac_encoder_factory,
         critic_encoder_factory=ac_encoder_factory,
-        observation_scaler=d3rlpy.preprocessing.StandardObservationScaler(),
-        action_scaler=d3rlpy.preprocessing.MinMaxActionScaler(),
-        reward_scaler=d3rlpy.preprocessing.MinMaxRewardScaler(),
+        observation_scaler=d3rlpy.preprocessing.StandardObservationScaler(mean=np.array(OBSERVATION_MEAN), std=np.array(OBSERVATION_STD)),
+        action_scaler=d3rlpy.preprocessing.MinMaxActionScaler(minimum=ACTION_MIN, maximum=ACTION_MAX),
+        reward_scaler=d3rlpy.preprocessing.MinMaxRewardScaler(minimum=REWARD_MIN, maximum=REWARD_MAX),
         q_func_factory=d3rlpy.models.q_functions.QRQFunctionFactory(n_quantiles=32),
     ).create(device=params['device'])
 
@@ -414,9 +499,9 @@ def createSAC(params):
         initial_temperature=_initial_temperature,
         actor_encoder_factory=ac_encoder_factory,
         critic_encoder_factory=ac_encoder_factory,
-        observation_scaler=d3rlpy.preprocessing.StandardObservationScaler(),
-        action_scaler=d3rlpy.preprocessing.MinMaxActionScaler(),
-        reward_scaler=d3rlpy.preprocessing.MinMaxRewardScaler()
+        observation_scaler=d3rlpy.preprocessing.StandardObservationScaler(mean=np.array(OBSERVATION_MEAN), std=np.array(OBSERVATION_STD)),
+        action_scaler=d3rlpy.preprocessing.MinMaxActionScaler(minimum=ACTION_MIN, maximum=ACTION_MAX),
+        reward_scaler=d3rlpy.preprocessing.MinMaxRewardScaler(minimum=REWARD_MIN, maximum=REWARD_MAX)
     ).create(device=params['device'])
 
     return sac
@@ -456,9 +541,9 @@ def createBCQ(params):
         rl_start_step=_rl_start_step,
         actor_encoder_factory=ac_encoder_factory,
         critic_encoder_factory=ac_encoder_factory,
-        observation_scaler=d3rlpy.preprocessing.StandardObservationScaler(),
-        action_scaler=d3rlpy.preprocessing.MinMaxActionScaler(),
-        reward_scaler=d3rlpy.preprocessing.MinMaxRewardScaler()
+        observation_scaler=d3rlpy.preprocessing.StandardObservationScaler(mean=np.array(OBSERVATION_MEAN), std=np.array(OBSERVATION_STD)),
+        action_scaler=d3rlpy.preprocessing.MinMaxActionScaler(minimum=ACTION_MIN, maximum=ACTION_MAX),
+        reward_scaler=d3rlpy.preprocessing.MinMaxRewardScaler(minimum=REWARD_MIN, maximum=REWARD_MAX)
     ).create(device=params['device'])
 
     return bcq
@@ -496,9 +581,9 @@ def createDT(params):
         activation_type=_activation_type,
         warmup_steps=_warmup_steps,
         clip_grad_norm=_clip_grad_norm,
-        observation_scaler=d3rlpy.preprocessing.StandardObservationScaler(),
-        action_scaler=d3rlpy.preprocessing.MinMaxActionScaler(),
-        reward_scaler=d3rlpy.preprocessing.MinMaxRewardScaler()
+        observation_scaler=d3rlpy.preprocessing.StandardObservationScaler(mean=np.array(OBSERVATION_MEAN), std=np.array(OBSERVATION_STD)),
+        action_scaler=d3rlpy.preprocessing.MinMaxActionScaler(minimum=ACTION_MIN, maximum=ACTION_MAX),
+        reward_scaler=d3rlpy.preprocessing.MinMaxRewardScaler(minimum=REWARD_MIN, maximum=REWARD_MAX)
     ).create(device=params['device'])
 
     return dt
--- findMeanStd.py
+++ findMeanStd.py
@@ -6,7 +6,6 @@ import time
 import numpy as np
 from tqdm import tqdm
 from concurrent.futures import ProcessPoolExecutor, as_completed
-from BweReward import reward_qoe_v1
 
 def load_data(datafile: os.PathLike | str) -> Optional[Dict]:
     try:
@@ -27,26 +26,18 @@ def load_train_data(
     if data is None:
         return None
 
-    bandwidth_predictions = np.array(data['bandwidth_predictions'])
     observations = np.array(data['observations'])
-    video_quality = np.array(data['video_quality'])
-    audio_quality = np.array(data['audio_quality'])
-    return observations, bandwidth_predictions, video_quality, audio_quality
+
+    return observations
 
 
 def process_file(filename: str) -> (np.ndarray, np.ndarray, np.ndarray, np.ndarray):
     # load the log file and prepare the dataset
-    observations_file, actions_file, _, _ = load_train_data(filename)
-    rewards_file = [reward_qoe_v1(o) for o in observations_file]
-    assert len(observations_file) > 0, f"File {filename} is empty"
-    obs_min = np.min(observations_file, axis=0)
-    obs_max = np.max(observations_file, axis=0)
-    act_min = np.min(actions_file)
-    act_max = np.max(actions_file)
-    rwd_min = np.min(rewards_file)
-    rwd_max = np.max(rewards_file)
+    observations_file = load_train_data(filename)
 
-    return obs_min, obs_max, act_min, act_max, rwd_min, rwd_max
+    assert len(observations_file) > 0, f"File {filename} is empty"
+
+    return observations_file
 
 
 def main() -> None:
@@ -56,9 +47,8 @@ def main() -> None:
     parser.add_argument("-o", "--odir", type=str, default="../data_np")
     args = parser.parse_args()
 
-    # load the list of log files under the given directory
     train_data_files = []
-    
+    # load the list of log files under the given directory
     # iterate over files in that directory
     files = sorted(os.listdir(args.idir))
     for name in files:
@@ -67,6 +57,8 @@ def main() -> None:
         if os.path.isfile(f):
             train_data_files.append(f)
 
+    # load the list of log files under the given directory
+    # iterate over files in that directory
     files = sorted(os.listdir(args.edir))
     for name in files:
         f = os.path.join(args.edir, name)
@@ -74,57 +66,39 @@ def main() -> None:
         if os.path.isfile(f):
             train_data_files.append(f)
 
+
     print(f"Files to load: {len(train_data_files)}")
 
     t_start = time.process_time()
 
-    observation_min = np.zeros(shape=(150,))
-    observation_max = np.zeros(shape=(150,))
-    action_min = 0.0
-    action_max = 0.0
-    reward_min = 0.0
-    reward_max = 0.0
+    observations = []
+    
     # load data log files
     with ProcessPoolExecutor() as executor:
         futures = [executor.submit(process_file, filename) for filename in train_data_files]
         for future in tqdm(as_completed(futures), desc=f'Loading', unit="file"):
-            obs_min, obs_max, act_min, act_max, rwd_min, rwd_max = future.result()
-            observation_min = np.minimum(observation_min, obs_min)
-            observation_max = np.maximum(observation_max, obs_max)
-            action_min = min(action_min, act_min)
-            action_max = max(action_max, act_max)
-            reward_min = min(reward_min, rwd_min)
-            reward_max = max(reward_max, rwd_max)
+            obs_file = future.result()
+            observations.append(obs_file)
 
-
+    observations = np.concatenate(observations)
+    obs_mean = observations.mean(axis=0)
+    obs_std = observations.std(axis=0)
     # create the file
-    f_name = f"{args.odir}/minmaxvalues.npz"
+    f_name = f"{args.odir}/meanstdvalues.npz"
     with open(f_name, 'wb') as f_o:
         np.savez_compressed(f_o,
-                            obs_min=observation_min,
-                            obs_max=observation_max,
-                            act_min=action_min,
-                            act_max=action_max,
-                            rwd_min=reward_min,
-                            rwd_max=reward_max)
+                            obs_mean=obs_mean,
+                            obs_std=obs_std)
 
     # elapsed time
     t_end = time.process_time()
-    print(f"Saving min and max values to {f_name}. Time used: {t_end - t_start} (s)")
+    print(f"Saving mean and std in {f_name}. {t_end - t_start} (s) used")
 
     with np.load(f_name, 'rb') as loaded:
-        print("obs min: ")
-        print(loaded['obs_min'])
-        print("obs max: ")
-        print(loaded['obs_max'])
-        print("act min: ")
-        print(loaded['act_min'])
-        print("act max: ")
-        print(loaded['act_max'])
-        print("rwd min: ")
-        print(loaded['rwd_min'])
-        print("rwd max: ")
-        print(loaded['rwd_max'])
+        print("obs mean: ")
+        print(loaded['obs_mean'])
+        print("obs std: ")
+        print(loaded['obs_std'])
 
 if __name__ == "__main__":
     main()
