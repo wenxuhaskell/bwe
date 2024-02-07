b50f34856429255d225093890cc4e826e9948bea
wen <wenxukth@gmail.com>
2024-02-07 20:56:24 +0100

    Small fix

--- BweModels.py
+++ BweModels.py
@@ -531,8 +531,8 @@ def createCQL(params):
     _temp_learning_rate = params["temp_learning_rate"]
     _alpha_learning_rate = params["alpha_learning_rate"]
 
-    ac_encoder_factory = d3rlpy.models.encoders.VectorEncoderFactory(hidden_units=[256,256,256])
-#    ac_encoder_factory = BweEncoder.LSTMEncoderFactory(32)
+#    ac_encoder_factory = d3rlpy.models.encoders.VectorEncoderFactory(hidden_units=[256,256,256])
+    ac_encoder_factory = BweEncoder.LSTMEncoderFactory(32)
     cql = d3rlpy.algos.CQLConfig(
         batch_size=_batch_size,
         gamma=_gamma,
